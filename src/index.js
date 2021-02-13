1fe3ce7028d53a85fb7ac0894cff3c9c0f86d6d6
Titania01 <adegbitebidmus@gmail.com>
2021-02-13 13:14:59 +0100

    feat: update code base

--- src/index.js
+++ src/index.js
@@ -12,6 +12,9 @@ const Hello = (props) => {
       <p>
         Hello {props.name}, you are {props.age} years old
       </p>
+  
+        
+  <p>So you were probably born in {bornYear()}</p>
       
     </div>
   )

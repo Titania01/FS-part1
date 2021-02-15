82c8744ab280b336b868f18d6293ec9413f46318
Titania01 <adegbitebidmus@gmail.com>
2021-02-15 12:10:29 +0100

    feat: complex state

--- src/index.js
+++ src/index.js
@@ -5,28 +5,20 @@ import Button from "./components/Button"
 
 
 const App = (props) => {
-  const [ counter, setCounter ] = useState(0) 
-
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const [left, setLeft] = useState(0) 
+  const [right, setRight] = useState(0)
+  
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button 
-        handleClick={ increaseByOne } 
-        text='plus'
-      />
-      <Button 
-        handleClick={ setToZero } 
-        text='zero'
-      />
-      <Button 
-      handleClick={ decreaseByOne } 
-      text='minus'
-    />
-        
+      {left}
+      <button onClick={() => setLeft(left + 1)} >
+        left
+      </button>        
+      <button onClick={() => setRight(right + 1)}>
+        right
+      </button>
+      {right}
     </div>
   );
 };

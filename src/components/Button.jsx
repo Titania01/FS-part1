969654ad387846d064fcc5d7079ce097e3340a49
Titania01 <adegbitebidmus@gmail.com>
2021-02-15 11:06:41 +0100

    feat: event handler

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleClick}>
+        {props.text}
+      </button>
+    </div>
+  )
+}
+
+export default Button
--- src/index.js
+++ src/index.js
@@ -1,40 +1,32 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import Display from "./components/Display"
+import Button from "./components/Button"
 
-// const Hello = ({ name, age }) => {
-  
-//   const bornYear = () => new Date().getFullYear() - age;
-
-//   return (
-//     <div>
-//       <p>
-//         Hello {name}, you are {age} years old
-//       </p>
-
-//       <p>So you were probably born in {bornYear()}</p>
-//     </div>
-//   );
-// };
 
 const App = (props) => {
   const [ counter, setCounter ] = useState(0) 
 
   const increaseByOne = () => setCounter(counter + 1)
-  
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
+
   return (
     <div>
-      {/* <h1>Greetings</h1> */}
-      {/* <Hello name="Maya" age={26 + 10} /> */}
-      {/* <Hello name={name} age={age} /> */}
-      {counter}
-    
-      <button onClick={ increaseByOne }>
-        plus
-      </button>
-      <button onClick={ setToZero } >
-        zero
-      </button>
+      <Display counter={counter} />
+      <Button 
+        handleClick={ increaseByOne } 
+        text='plus'
+      />
+      <Button 
+        handleClick={ setToZero } 
+        text='zero'
+      />
+      <Button 
+      handleClick={ decreaseByOne } 
+      text='minus'
+    />
+        
     </div>
   );
 };

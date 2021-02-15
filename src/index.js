ca9b20858826dfe0c303d3e857bd5da4512750af
Titania01 <adegbitebidmus@gmail.com>
2021-02-15 17:16:50 +0100

    feat: conditional rendering

--- src/index.js
+++ src/index.js
@@ -1,40 +1,32 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import History from "./History"
+import Button from "./Button"
 
 
 
-const App = (props) => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0 
-  }) 
+const App = () => {
+  const [left, setLeft] = useState(0) 
+  const [right, setRight] = useState(0) 
+  const [allClicks, setAll] = useState([])
 
   const handleLeftClick = () => {
-    const newClicks = {
-      ...clicks,
-      left: clicks.left + 1 
-    }
-    setClicks(newClicks)
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
   }
 
   const handleRightClick = () => {
-    const newClicks = {
-      ...clicks,
-      right: clicks.right + 1
-    }
-    setClicks(newClicks)
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
   }
   
-
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick} >
-        left
-      </button>        
-      <button onClick={handleRightClick}>
-        right
-      </button>
-      {clicks.right}
+      {left}<Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+    
     </div>
   );
 };
